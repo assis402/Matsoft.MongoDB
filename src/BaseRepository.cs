0fb4041226c542679c37e0a05ba57924a6ffed9c
Matheus de Assis <assis4002@gmail.com>
2023-12-14 23:27:01 -0300

    fix: add methods with session and wihtout session

--- src/BaseRepository.cs
+++ src/BaseRepository.cs
@@ -9,35 +9,95 @@ public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : 
 
     protected BaseRepository(BaseContextDb context)
         => _entityCollection = context.Database.GetCollection<TEntity>(name: Utils.GetCollectionName<TEntity>());
+    
+    public async Task InsertOneAsync(TEntity entity)
+        => await _entityCollection.InsertOneAsync(document: entity);
+    
+    public async Task InsertAsync(IEnumerable<TEntity> entityList)
+        => await _entityCollection.InsertManyAsync(entityList);
+    
+    public async Task InsertManyAsync(params TEntity[] entityList)
+        => await _entityCollection.InsertManyAsync(entityList);
+    
+    public async Task<TEntity> FindByIdAsync(string id)
+    {
+        var filter = BaseEntity.FindByIdDefinition<TEntity>(id);
+        return await FindOneAsync(filter); 
+    }
+    
+    public async Task<IEnumerable<TEntity>> FindAsync(FilterDefinition<TEntity> filterDefinition)
+    {
+        var result = await _entityCollection.FindAsync(filterDefinition);
+        return await result.ToListAsync();
+    }
 
-    public virtual async Task InsertOneAsync(TEntity entity, IClientSessionHandle session = null)
+    public async Task<TEntity> FindOneAsync(FilterDefinition<TEntity> filterDefinition)
+    {
+        var result = await _entityCollection.FindAsync(filterDefinition);
+        return await result.FirstOrDefaultAsync();
+    }
+
+    public async Task<bool> Exists(FilterDefinition<TEntity> filterDefinition)
+    {
+        var result = await _entityCollection.CountDocumentsAsync(filterDefinition);
+        return result > 0;
+    }
+
+    public async Task UpdateOneAsync(TEntity entity,
+        UpdateDefinition<TEntity> updateDefinition)
+    {
+        updateDefinition = entity.SetUpdateDateAndGetDefinition(updateDefinition);
+        await _entityCollection.UpdateOneAsync(entity.FindByIdDefinition<TEntity>(), updateDefinition);
+    }
+
+    public async Task UpdateOneAsync(string id,
+        UpdateDefinition<TEntity> updateDefinition)
+    {
+        updateDefinition = BaseEntity.UpdateDateDefinition(updateDefinition);
+        await _entityCollection.UpdateOneAsync(BaseEntity.FindByIdDefinition<TEntity>(id), updateDefinition);
+    }
+
+    public async Task UpdateOneAsync(FilterDefinition<TEntity> filterDefinition,
+        UpdateDefinition<TEntity> updateDefinition)
+    {
+        updateDefinition = BaseEntity.UpdateDateDefinition(updateDefinition);
+        await _entityCollection.UpdateOneAsync(filterDefinition, updateDefinition);
+    }
+
+    public async Task DeleteOneAsync(string id)
+    {
+        var filterDefinition = BaseEntity.FindByIdDefinition<TEntity>(id);
+        await _entityCollection.DeleteOneAsync(filterDefinition);
+    }
+    
+    public virtual async Task InsertOneAsync(TEntity entity, IClientSessionHandle session)
         => await _entityCollection.InsertOneAsync(document: entity, session: session);
     
-    public virtual async Task InsertAsync(IEnumerable<TEntity> entityList, IClientSessionHandle session = null)
+    public virtual async Task InsertAsync(IEnumerable<TEntity> entityList, IClientSessionHandle session)
         => await _entityCollection.InsertManyAsync(documents: entityList, session: session);
     
-    public virtual async Task InsertManyAsync(IClientSessionHandle session = null, params TEntity[] entityList)
+    public virtual async Task InsertManyAsync(IClientSessionHandle session, params TEntity[] entityList)
         => await _entityCollection.InsertManyAsync(documents: entityList, session: session);
     
-    public virtual async Task<TEntity> FindByIdAsync(string id, IClientSessionHandle session = null)
+    public virtual async Task<TEntity> FindByIdAsync(string id, IClientSessionHandle session)
     {
         var filter = BaseEntity.FindByIdDefinition<TEntity>(id: id);
         return await FindOneAsync(filterDefinition: filter, session: session); 
     }
     
-    public virtual async Task<IEnumerable<TEntity>> FindAsync(FilterDefinition<TEntity> filterDefinition, IClientSessionHandle session = null)
+    public virtual async Task<IEnumerable<TEntity>> FindAsync(FilterDefinition<TEntity> filterDefinition, IClientSessionHandle session)
     {
         var result = await _entityCollection.FindAsync(filter: filterDefinition, session: session);
         return await result.ToListAsync();
     }
 
-    public virtual async Task<TEntity> FindOneAsync(FilterDefinition<TEntity> filterDefinition, IClientSessionHandle session = null)
+    public virtual async Task<TEntity> FindOneAsync(FilterDefinition<TEntity> filterDefinition, IClientSessionHandle session)
     {
         var result = await _entityCollection.FindAsync(filter: filterDefinition, session: session);
         return await result.FirstOrDefaultAsync();
     }
 
-    public virtual async Task<bool> Exists(FilterDefinition<TEntity> filterDefinition, IClientSessionHandle session = null)
+    public virtual async Task<bool> Exists(FilterDefinition<TEntity> filterDefinition, IClientSessionHandle session)
     {
         var result = await _entityCollection.CountDocumentsAsync(filter: filterDefinition, session: session);
         return result > 0;
@@ -45,7 +105,7 @@ public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : 
 
     public virtual async Task UpdateOneAsync(TEntity entity,
         UpdateDefinition<TEntity> updateDefinition,
-        IClientSessionHandle session = null)
+        IClientSessionHandle session)
     {
         updateDefinition = entity.SetUpdateDateAndGetDefinition(definition: updateDefinition);
         await _entityCollection.UpdateOneAsync(filter: entity.FindByIdDefinition<TEntity>(), update: updateDefinition, session: session);
@@ -53,7 +113,7 @@ public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : 
 
     public virtual async Task UpdateOneAsync(string id,
         UpdateDefinition<TEntity> updateDefinition,
-        IClientSessionHandle session = null)
+        IClientSessionHandle session)
     {
         updateDefinition = BaseEntity.UpdateDateDefinition(definition: updateDefinition);
         await _entityCollection.UpdateOneAsync(filter: BaseEntity.FindByIdDefinition<TEntity>(id: id), update: updateDefinition, session: session);
@@ -61,13 +121,13 @@ public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : 
 
     public virtual async Task UpdateOneAsync(FilterDefinition<TEntity> filterDefinition,
         UpdateDefinition<TEntity> updateDefinition,
-        IClientSessionHandle session = null)
+        IClientSessionHandle session)
     {
         updateDefinition = BaseEntity.UpdateDateDefinition(definition: updateDefinition);
         await _entityCollection.UpdateOneAsync(filter: filterDefinition, update: updateDefinition, session: session);
     }
 
-    public virtual async Task DeleteOneAsync(string id, IClientSessionHandle session = null)
+    public virtual async Task DeleteOneAsync(string id, IClientSessionHandle session)
     {
         var filterDefinition = BaseEntity.FindByIdDefinition<TEntity>(id: id);
         await _entityCollection.DeleteOneAsync(filter: filterDefinition, session: session);
--- src/IBaseRepository.cs
+++ src/IBaseRepository.cs
@@ -4,25 +4,47 @@ namespace Matsoft.MongoDB;
 
 public interface IBaseRepository<TEntity> where TEntity : BaseEntity
 {
-    public Task InsertOneAsync(TEntity entity, IClientSessionHandle session = null);
+    public Task InsertOneAsync(TEntity entity);
     
-    public Task InsertAsync(IEnumerable<TEntity> entityList, IClientSessionHandle session = null);
+    public Task InsertAsync(IEnumerable<TEntity> entityList);
+    
+    public Task InsertManyAsync(params TEntity[] entityList);
+    
+    public Task<TEntity> FindByIdAsync(string id);
+    
+    public Task<IEnumerable<TEntity>> FindAsync(FilterDefinition<TEntity> filterDefinition);
+
+    public Task<TEntity> FindOneAsync(FilterDefinition<TEntity> filterDefinition);
+
+    public Task<bool> Exists(FilterDefinition<TEntity> filterDefinition);
+
+    public Task UpdateOneAsync(TEntity entity, UpdateDefinition<TEntity> updateDefinition);
+
+    public Task UpdateOneAsync(string id, UpdateDefinition<TEntity> updateDefinition);
+
+    public Task UpdateOneAsync(FilterDefinition<TEntity> filterDefinition, UpdateDefinition<TEntity> updateDefinition);
+
+    public Task DeleteOneAsync(string id);
+    
+    public Task InsertOneAsync(TEntity entity, IClientSessionHandle session);
+    
+    public Task InsertAsync(IEnumerable<TEntity> entityList, IClientSessionHandle session);
     
     public Task InsertManyAsync(IClientSessionHandle session = null, params TEntity[] entityList);
     
-    public Task<TEntity> FindByIdAsync(string id, IClientSessionHandle session = null);
+    public Task<TEntity> FindByIdAsync(string id, IClientSessionHandle session);
     
-    public Task<IEnumerable<TEntity>> FindAsync(FilterDefinition<TEntity> filterDefinition, IClientSessionHandle session = null);
+    public Task<IEnumerable<TEntity>> FindAsync(FilterDefinition<TEntity> filterDefinition, IClientSessionHandle session);
 
-    public Task<TEntity> FindOneAsync(FilterDefinition<TEntity> filterDefinition, IClientSessionHandle session = null);
+    public Task<TEntity> FindOneAsync(FilterDefinition<TEntity> filterDefinition, IClientSessionHandle session);
 
-    public Task<bool> Exists(FilterDefinition<TEntity> filterDefinition, IClientSessionHandle session = null);
+    public Task<bool> Exists(FilterDefinition<TEntity> filterDefinition, IClientSessionHandle session);
 
-    public Task UpdateOneAsync(TEntity entity, UpdateDefinition<TEntity> updateDefinition, IClientSessionHandle session = null);
+    public Task UpdateOneAsync(TEntity entity, UpdateDefinition<TEntity> updateDefinition, IClientSessionHandle session);
 
-    public Task UpdateOneAsync(string id, UpdateDefinition<TEntity> updateDefinition, IClientSessionHandle session = null);
+    public Task UpdateOneAsync(string id, UpdateDefinition<TEntity> updateDefinition, IClientSessionHandle session);
 
-    public Task UpdateOneAsync(FilterDefinition<TEntity> filterDefinition, UpdateDefinition<TEntity> updateDefinition, IClientSessionHandle session = null);
+    public Task UpdateOneAsync(FilterDefinition<TEntity> filterDefinition, UpdateDefinition<TEntity> updateDefinition, IClientSessionHandle session);
 
-    public Task DeleteOneAsync(string id, IClientSessionHandle session = null);
+    public Task DeleteOneAsync(string id, IClientSessionHandle session);
 }
